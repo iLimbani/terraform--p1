f2f85568227e41f97f8ee0e2d7cf8949ac1688d0
iLimbani <134752486+iLimbani@users.noreply.github.com>
2023-10-21 23:45:06 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "ami_value" {
+  description = "ami value"
+}
+
+variable "instance_type_value" {
+  description = "description of instance"
+}
+
+variable "cidr_block_value" {
+  description = "range of cidr block"
+}
+
